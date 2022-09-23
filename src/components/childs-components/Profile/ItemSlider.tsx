aa87e5336c2919325f0a1179c1317e79535c4f17
Fattah.Behmadi <fatahbepro@gmail.com>
2022-09-23 15:01:47 +0330

    add audio to slide

--- src/components/childs-components/Profile/ItemSlider.tsx
+++ src/components/childs-components/Profile/ItemSlider.tsx
@@ -1,18 +1,17 @@
 import { Box } from "@mui/material";
+import { BoxProps } from "@mui/material/Box";
 import React from "react";
 
-export default function ItemSlider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function ItemSlider(props: BoxProps) {
   return (
     <Box
+      {...props}
       sx={{
         height: "20rem",
+        ...props.sx
       }}
     >
-      {children}
+      {props.children}
     </Box>
   );
 }
--- src/components/childs-components/Profile/Slider.tsx
+++ src/components/childs-components/Profile/Slider.tsx
@@ -4,7 +4,7 @@ import ItemSlider from "./ItemSlider";
 import ImageZoom from "./ImageZoom";
 
 export default function Slider() {
-  const [bg, setbg] = useState<string>('');
+  const [bg, setbg] = useState<string>("");
 
   useEffect(() => {
     import("../../../assets/img/bg.jpg").then((image) => setbg(image.default));
@@ -14,10 +14,30 @@ export default function Slider() {
     <>
       <BaseCarousel>
         <ItemSlider>
-            <ImageZoom imagePath={bg} imgZoom={bg} />
+          <ImageZoom imagePath={bg} imgZoom={bg} />
         </ItemSlider>
-        <ItemSlider>
-          <p>slide 2</p>
+        <ItemSlider
+          sx={{
+            display: "grid",
+            placeItems: "center",
+          }}
+        >
+          <figure
+            style={{
+              display: "grid",
+              placeItems: "center",
+            }}
+          >
+            <figcaption>موزیک </figcaption>
+            <audio
+              controls
+              src="https://cdns-preview-2.dzcdn.net/stream/c-298a6d9e5299f71f7c8e7d29e7f874ff-5.mp3"
+            >
+              <a href="https://cdns-preview-2.dzcdn.net/stream/c-298a6d9e5299f71f7c8e7d29e7f874ff-5.mp3">
+                Download audio
+              </a>
+            </audio>
+          </figure>
         </ItemSlider>
       </BaseCarousel>
     </>
